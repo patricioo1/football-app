14d2583152caa92cf99b44e533d98bf89b673fe5
Patryk Smolarz <patryksmolarz1@gmail.com>
2022-10-19 18:30:44 +0200

    remove standings saga

--- src/components/API/API.js
+++ src/components/API/API.js
@@ -46,23 +46,6 @@ export const getMatchday = async (ids, date) => {
   return null;
 }
 
-export const getSingleLeagueStandings = async id => {
-  const response = await fetch(
-    new Request(
-      `https://api.football-data.org/v2/competitions/${id}/standings`,
-      {
-        headers: {
-          'X-Auth-Token': `${process.env.REACT_APP_API_KEY}`,
-        },
-      }
-    )
-  )
-  if (response.status === 200) {
-    return response.json()
-  }
-  return null;
-}
-
 export const getBestScorers = async code => {
   const response = await fetch(
     new Request(
--- src/redux/allleagues/reducers/singleLeagueInfo/singleLeagueStandings/actionTypes.ts
+++
@@ -1,3 +0,0 @@
-import * as actionTypes from '../../../actions/actions'
-
-export type StandingsActions = ReturnType<typeof actionTypes.fetchSingleLeagueStandings> | ReturnType<typeof actionTypes.setSingleLeagueStandings> | ReturnType<typeof actionTypes.singleLeagueStandingsFailure>
